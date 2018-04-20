13a4595d937fa669ea397ed73f7194ee46118e80
X Guo <3428041+xlyytcy@users.noreply.github.com>
2018-04-20 04:29:51 -0400

    Remove sensitive information

--- testPyqt5.py
+++ testPyqt5.py
@@ -1,9 +1,9 @@
 from PyQt5.QtSql import *
 
 db = QSqlDatabase.addDatabase('QMYSQL')
-db.setHostName("yelpdb.clzycvghm6ps.us-east-2.rds.amazonaws.com")
+db.setHostName('yelpdb.clzycvghm6ps.us-east-2.rds.amazonaws.com')
 
 db.setDatabaseName(input('DB name is: '))
 db.setUserName(input('User name is: '))
 db.setPassword(input('PW is: '))
-db.open()
+print(db.open())
