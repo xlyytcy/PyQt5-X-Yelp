1da3ceafe3d36444c7206de38677881b2c5a19fd
X Guo <3428041+xlyytcy@users.noreply.github.com>
2018-04-22 23:11:38 -0400

    Update of UI file.

--- testPyqt5.py
+++ testPyqt5.py
@@ -1,4 +1,8 @@
 from PyQt5.QtSql import *
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
 
 db = QSqlDatabase.addDatabase('QMYSQL')
 db.setHostName('yelpdb.clzycvghm6ps.us-east-2.rds.amazonaws.com')
@@ -7,3 +11,11 @@ db.setDatabaseName(input('DB name is: '))
 db.setUserName(input('User name is: '))
 db.setPassword(input('PW is: '))
 print(db.open())
+
+
+#query = QSqlQuery(db)
+#query.exec("SELECT name, salary FROM employee WHERE salary > 50000");
+
+sqlTableModel = QSqlTableModel(db)
+sqlTableModel.setTable('user')
+
